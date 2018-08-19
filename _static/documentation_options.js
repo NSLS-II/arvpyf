5d7fd7f89e0072f6fd1a8f644cd4eb6fe327601f
Travis CI User <travis@example.org>
2018-08-19 23:05:18 +0000

    Update docs after building Travis build 6 of SciIO/arvpyf
    The docs were built from the branch 'master' against the commit
    5470a626c3402fca99359e8d4499e88bfecde386.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/SciIO/arvpyf/jobs/418017704.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0.post1+g8ac0052',
+    VERSION: '0.1.0.post2+g5470a62',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
