dd7ec600a471d58515c20f50506cfd762b7b05ed
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-06-05 18:25:15 +0000

    Update docs after building Travis build 21 of NSLS-II/arvpyf
    The docs were built from the branch 'master' against the commit
    63b7ee9faa4e4fe40bffd0de7f150a5f61334f49.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/NSLS-II/arvpyf/jobs/541892052.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.0.post4+g17f9f62',
+    VERSION: '0.4.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
